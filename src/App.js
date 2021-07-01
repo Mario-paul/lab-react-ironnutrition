bec4a6f78e154c12eda99119dd0b4cf164e3e3be
Mario P. Cardenas <15961362+Mario-paul@users.noreply.github.com>
2021-06-30 22:17:57 -0400

    iteration 0 done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//iteration 0 -- import
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+
 function App() {
   return (
     <div className="App">
